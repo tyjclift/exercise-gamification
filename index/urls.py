29d1b287836a7c0a65acad04e53ffc0030acd271
alannulli <alanlicmc@gmail.com>
2021-03-29 21:07:15 -0400

    not working, initial changes

--- index/urls.py
+++ index/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 
 from . import views
 
+# app_name = 'index'
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("accounts/", include("allauth.urls")),
+    path("upper/", views.UpperBodyView.as_view(), name="upper"),
 ]
