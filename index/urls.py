4da00ceaeb5e4e215cc08e4da4a316b6b1859099
Kedar Kulkarni <15352802+Kedar-K15@users.noreply.github.com>
2021-04-03 19:31:41 -0400

    Add functionality to update sets and reps

--- index/urls.py
+++ index/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("cardio/", views.CardioView.as_view(), name="cardio"),
     path("lower/", views.LowerBodyView.as_view(), name="lower"),
-    path("upper/", views.UpperBodyView.as_view(), name="upper"),
+   # path("upper/", views.UpperBodyView.as_view(), name="upper"),
+    path("upper/", views.UpperBodyView, name="upper"),
 ]
--- index/views.py
+++ index/views.py
@@ -18,10 +18,42 @@ class LowerBodyView(CreateView):
     fields = ['squats', 'lunges', 'calf_raises', 'leg_press', 'deadlifts']
     success_url = "index/"
 
-class UpperBodyView(CreateView):
-    model = UpperBody
-    fields = ['pushups', 'pullups', 'back_row', 'bicep_curl', 'tricep_extension']
-    success_url = "index/"
+# class UpperBodyView(CreateView):
+#     model = UpperBody
+#     fields = ['pushups', 'pullups', 'back_row', 'bicep_curl', 'tricep_extension']
+#     success_url = "index/"
+
+
+def UpperBodyView(request):
+    form = UpperBodyForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        querey_list = UpperBody.objects.all()
+        return render(request, 'index.html', {'querey_list': querey_list})
+    context = {'form': form }
+        
+    return render(request, 'index/upperbody_form.html', context)
+
+# def parse_thoughts(request):
+# 	thoughts_var = Thoughts(title_text=request.POST['title_text'], 
+# 		deep_thought_text=request.POST['deep_thought_text'])
+# 	thoughts_var.save()
+
+# 	return HttpResponseRedirect(reverse('polls:thoughts'))
+
+
+# def upper_body_list(request):
+# 	querey_list = UpperBodyForm.objects.all()
+# 	print(querey_list)
+# 	return render(request, 'index.html', {'querey_list': querey_list})
+
+
+
+# class CreateUpperBodyView(CreateView):
+#     model = UpperBody
+#     form_class = UpperBodyForm
+#     # template_name = 'index/upperbody_form.html'
+#     success_url = '/index'
 
 # class ResultView(generic.ListView):
 #     # model = 
