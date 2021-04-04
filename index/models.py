1d290af40aa3188d0602c31728c2499c8ffeeaf9
alannulli <alanlicmc@gmail.com>
2021-04-03 22:29:42 -0400

    added code to implement set/rep counters

--- index/models.py
+++ index/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
 
@@ -15,9 +16,29 @@ class LowerBody(models.Model):
     leg_press = models.CharField(max_length=4)
     deadlifts = models.CharField(max_length=4)
 
+
+# class Upper(models.Model):
+#     upper_workout = models.CharField(choices=UPPER_WORKOUTS)
+#     workout_counter = models.SmallIntegerField(default=0)
+
 class UpperBody(models.Model):
-    pushups = models.CharField(max_length=4)
-    pullups = models.CharField(max_length=4)
-    back_row = models.CharField(max_length=4)
-    bicep_curl = models.CharField(max_length=4)
-    tricep_extension = models.CharField(max_length=4)
+    UPPER_WORKOUTS = (('pushups', 'pushups'),)
+    upper_workout = models.CharField(max_length=10, choices=UPPER_WORKOUTS, default='pushups')
+    reps_count = models.SmallIntegerField(default=0)
+    sets_count = models.SmallIntegerField(default=0)
+    reps = models.SmallIntegerField(default=0)
+    sets = models.SmallIntegerField(default=0)
+    # reps = models.SmallIntegerField(max_length=4, default='')
+    # sets = models.SmallIntegerField(max_length=4, default='')
+
+class UpperBodyForm(forms.ModelForm):
+    class Meta:
+        model = UpperBody
+        fields = ['sets','reps']
+
+# class UpperBody(models.Model):
+#     pushups = models.CharField(max_length=4)
+#     # pullups = models.CharField(max_length=4)
+#     # back_row = models.CharField(max_length=4)
+#     # bicep_curl = models.CharField(max_length=4)
+#     # tricep_extension = models.CharField(max_length=4)
--- index/views.py
+++ index/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from django.db.models import F
 from django.views.generic.edit import CreateView
 
 from .models import *
@@ -11,17 +12,44 @@ class IndexView(generic.TemplateView):
 class CardioView(CreateView):
     model = Cardio
     fields = ['running', 'biking', 'swimming', 'walking']
-    success_url = "index/"
+    success_url = "/index"
 
 class LowerBodyView(CreateView):
     model = LowerBody
     fields = ['squats', 'lunges', 'calf_raises', 'leg_press', 'deadlifts']
-    success_url = "index/"
+    success_url = "/index"
 
-class UpperBodyView(CreateView):
-    model = UpperBody
-    fields = ['pushups', 'pullups', 'back_row', 'bicep_curl', 'tricep_extension']
-    success_url = "index/"
+
+def UpperBodyView(request):
+    form = UpperBodyForm(request.POST or None)
+    if form.is_valid():
+        cd = form.cleaned_data
+        workout_log = UpperBody.objects.get_or_create(upper_workout='pushups')[0]
+        workout_log.reps_count = F('reps_count') + cd.get('reps')
+        workout_log.sets_count = F('sets_count') + cd.get('sets')
+        print(workout_log.reps_count)
+        context = { 'pushups':{'reps':workout_log.reps_count, 'sets':workout_log.sets_count} }
+
+    context = {'form': form}
+    return render(request, 'index.html/', context)
+
+    # context = {'reps':workout_log.reps, 'sets':workout_log.sets}
+    # return render(request, 'index.html', context)
+
+# def UpperBodyView(request):
+#     form = UpperBodyForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         querey_list = UpperBody.objects.all()
+#         return render(request, 'index.html', {'querey_list': querey_list})
+#     context = {'form': form }
+        
+#     return render(request, 'index/upperbody_form.html', context)
+
+# class UpperBodyView(CreateView):
+#     model = UpperBody
+#     fields = ['pushups']
+#     success_url = "/index"
 
 # class ResultView(generic.ListView):
 #     # model = 
