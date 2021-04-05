9b274ffa2c0aa43d4a961658e464c6218db2df9e
Kedar Kulkarni <15352802+Kedar-K15@users.noreply.github.com>
2021-04-05 13:16:41 -0400

    Fix filtering by user

--- index/views.py
+++ index/views.py
@@ -12,9 +12,17 @@ class IndexView(generic.TemplateView):
 def UpperBodyView(request):
     form = UpperBodyForm(request.POST or None)
     if form.is_valid():
-        form.save()
-        querey_list = UpperBody.objects.filter(current_user=request.user)
-        return render(request, 'index/upper_list.html', {'upper_querey_list': querey_list})
+    	form_user = form.save(commit=False)
+    	form_user.current_user = request.user
+    	
+    	
+    	form_user.save()
+    	form.save()
+    	
+    	querey_list = UpperBody.objects.filter(current_user=request.user)
+    	
+    	print(querey_list)
+    	return render(request, 'index/upper_list.html', {'upper_querey_list': querey_list})
     context = {'form': form}
 
     return render(request, 'index/upperbody_form.html', context)
@@ -23,7 +31,12 @@ def UpperBodyView(request):
 def CardioView(request):
     form = CardioForm(request.POST or None)
     if form.is_valid():
+        form_user = form.save(commit=False)
+        form_user.current_user = request.user
+
+        form_user.save()
         form.save()
+
         querey_list = Cardio.objects.filter(current_user=request.user)
         return render(request, 'index/cardio_list.html', {'cardio_querey_list': querey_list})
     context = {'form': form}
@@ -34,7 +47,12 @@ def CardioView(request):
 def LowerBodyView(request):
     form = LowerBodyForm(request.POST or None)
     if form.is_valid():
+        form_user = form.save(commit=False)
+        form_user.current_user = request.user
+
+        form_user.save()
         form.save()
+        
         querey_list = LowerBody.objects.filter(current_user=request.user)
         return render(request, 'index/lower_list.html', {'lower_querey_list': querey_list})
     context = {'form': form}
