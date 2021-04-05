82aef965c34dccdf1a75891b699ea3b0acd5b50a
Kedar Kulkarni <15352802+Kedar-K15@users.noreply.github.com>
2021-04-04 20:54:39 -0400

    Change render html pages

--- index/views.py
+++ index/views.py
@@ -7,44 +7,36 @@ from .models import *
 
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
-
-# class CardioView(CreateView):
-#     model = Cardio
-#     fields = ['running', 'biking', 'swimming', 'walking']
-#     success_url = "index/"
-
-# class LowerBodyView(CreateView):
-#     model = LowerBody
-#     fields = ['squats', 'lunges', 'calf_raises', 'leg_press', 'deadlifts']
-#     success_url = "index/"
-
+    
 # https://stackoverflow.com/questions/22739701/django-save-modelform
 def UpperBodyView(request):
     form = UpperBodyForm(request.POST or None)
     if form.is_valid():
         form.save()
         querey_list = UpperBody.objects.all()
-        return render(request, 'index.html', {'querey_list': querey_list})
-    context = {'form': form }
-        
+        return render(request, 'index/upper_list.html', {'upper_querey_list': querey_list})
+    context = {'form': form}
+
     return render(request, 'index/upperbody_form.html', context)
+
 
 def CardioView(request):
     form = CardioForm(request.POST or None)
     if form.is_valid():
         form.save()
         querey_list = Cardio.objects.all()
-        return render(request, 'index.html', {'querey_list': querey_list})
-    context = {'form': form }
-        
+        return render(request, 'index/cardio_list.html', {'cardio_querey_list': querey_list})
+    context = {'form': form}
+
     return render(request, 'index/cardio_form.html', context)
+
 
 def LowerBodyView(request):
     form = LowerBodyForm(request.POST or None)
     if form.is_valid():
         form.save()
         querey_list = LowerBody.objects.all()
-        return render(request, 'index.html', {'querey_list': querey_list})
-    context = {'form': form }
-        
+        return render(request, 'index/lower_list.html', {'lower_querey_list': querey_list})
+    context = {'form': form}
+
     return render(request, 'index/lowerbody_form.html', context)
