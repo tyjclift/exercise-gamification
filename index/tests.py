e0fd85065a03f4b868ad9968b04da763a3c6db14
Kedar Kulkarni <15352802+Kedar-K15@users.noreply.github.com>
2021-04-04 22:17:30 -0400

    Add initial tests

--- index/tests.py
+++ index/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
-
+from .models import *
 # Create your tests here.
+
+
+class UpperBodyTest(TestCase):
+	def test_is_valid(self):
+		test_form = UpperBodyForm(data={
+			'type': 'Pushups',
+			'reps': 1,
+			'sets': 1,
+			'date': '2021-01-01'
+		})
+		self.assertTrue(test_form.is_valid())
+
+class LowerBodyTest(TestCase):
+	def test_is_valid(self):
+		test_form = LowerBodyForm(data={
+			'type': 'Squats',
+			'reps': 1,
+			'sets': 1,
+			'date': '2021-01-01'
+		})
+		self.assertTrue(test_form.is_valid())
+
+class CardioTest(TestCase):
+	def test_is_valid(self):
+		test_form = CardioForm(data={
+			'type': 'Running',
+			'time': 0,
+			'distance': 0,
+			'date': '2021-01-01'
+		})
+		self.assertTrue(test_form.is_valid())
