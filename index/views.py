d6e18d161d5eb22fc27a5d32d725f85d94758a66
alannulli <alanlicmc@gmail.com>
2021-04-09 14:43:37 -0400

    fixed not logged in issue

--- index/views.py
+++ index/views.py
@@ -6,11 +6,14 @@ from django.views.generic.edit import CreateView
 from .models import *
 
 def IndexView(request):
-    context = {
-        'cardio_query_list': Cardio.objects.filter(current_user=request.user),
-        'upper_query_list': UpperBody.objects.filter(current_user=request.user),
-        'lower_query_list': LowerBody.objects.filter(current_user=request.user),
-    }
+    context = {}
+    if request.user.is_authenticated:
+        context = {
+            'cardio_query_list': Cardio.objects.filter(current_user=request.user),
+            'upper_query_list': UpperBody.objects.filter(current_user=request.user),
+            'lower_query_list': LowerBody.objects.filter(current_user=request.user),
+        }
+    
     return render(request, 'index/index.html', context)
 
 # https://stackoverflow.com/questions/22739701/django-save-modelform
