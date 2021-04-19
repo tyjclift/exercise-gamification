2f1ed6b078a472cce707e3de8fb4bf8da3b36d21
Pat Hourican <75340874+pathourican@users.noreply.github.com>
2021-04-19 17:43:40 -0400

    Styling
    Image appearance, additional changes in url.py and setting.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -65,6 +65,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
+TEMPLATE_DIRS = (
+   
+   BASE_DIR + '../index/templates',
+)
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
@@ -123,8 +127,23 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+#PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+#STATIC_URL = '/static/'
 
 # # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
